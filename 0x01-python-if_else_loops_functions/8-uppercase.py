f978ebbbfe53a4fbbf28a649b1d69688b0323dbf
Brehyma Traore <brehymaraore50@gmail.com>
2023-11-28 19:06:19 -0800

    some issues fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
     new_str = ""
+
     for s in str:
         if (s.isalpha):
-            new_str += chr(ord(s) - 32) if ('a' <= s <= 'z') else s
-            print("{}".format(new_str))
+            new_str += chr(ord(s) - 32) if ('a' <= s <= 'z') elses
         else:
             new_str += s
-            print("{}".format(new_str))
+        print("{}".format(new_str))
---
+++ 0x01-python-if_else_loops_functions/8.main.py
@@ -0,0 +1,6 @@
+
+def islower(c)
+decse = __import__('8-uppercase').uppercase
+
+uppercase("best")
+uppercase("Best School 98 Battery street"):islower(c)
