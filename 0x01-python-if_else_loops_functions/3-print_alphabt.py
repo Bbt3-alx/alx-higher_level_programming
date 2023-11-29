bea0237e7315ff4695c36e62594c20c793bad667
Brehyma Traore <brehymaraore50@gmail.com>
2023-11-28 17:27:44 -0800

    alphabet by excluding e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for alphabet in range(ord('a'), ord('z') + 1):
-    if ('q' != alphabet != 'e'):
+    if (ord('q') != alphabet != ord('e')):
         print("{}".format(chr(alphabet)), end="")
     else:
         continue
