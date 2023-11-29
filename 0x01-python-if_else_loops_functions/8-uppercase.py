e37569eb0e25d64ef7eec98de2d7aa0d44983efb
Brehyma Traore <brehymaraore50@gmail.com>
2023-11-28 18:47:20 -0800

    format specifer fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,7 @@ def uppercase(str):
     for s in str:
         if ('a' <= s <= 'z'):
             new_str += chr(ord(s) - 32)
+            print("{}".format(new_str))
         else:
             new_str += s
-    return new_str
+            print("{}".format(new_str))
