297b45a1353d90e01af64605f5e9f3a70bef1c95
Brehyma Traore <brehymaraore50@gmail.com>
2023-11-28 17:23:54 -0800

    print alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for alphabet in range(ord('a'), ord('z') + 1):
     if ('q' != alphabet != 'e'):
-        print(chr(alphabet), end="")
+        print("{}".format(chr(alphabet)), end="")
     else:
         continue
