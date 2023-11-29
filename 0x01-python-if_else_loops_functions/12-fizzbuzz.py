b2067dae589dfaa27a28b91d708dbd759d99f582
Brehyma Traore <brehymaraore50@gmail.com>
2023-11-29 15:10:40 -0800

    Syntax error fixed

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        elif (i % 3 and i % 5):
+        if (i % 3 and i % 5):
             print("FizzBuzz", end=" ")
 
-        if (i % 3 == 0):
+        elif (i % 3 == 0):
             print("Fizz", end=" ")
         elif (i % 5 == 0):
             print("Buzz", end=" ")
