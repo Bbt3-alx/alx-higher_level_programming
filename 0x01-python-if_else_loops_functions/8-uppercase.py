36830a241e629e11583bf31419d8ab790cf9d4cb
Brehyma Traore <brehymaraore50@gmail.com>
2023-11-28 18:43:08 -0800

    Task 8 issues fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    new_str = ""
     for s in str:
-        if ('A' <= s <= 'Z'):
-                print("{}".format(s))
+        if ('a' <= s <= 'z'):
+            new_str += chr(ord(s) - 32)
+        else:
+            new_str += s
+    return new_str
