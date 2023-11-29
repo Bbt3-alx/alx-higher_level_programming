1eaf56656fc585354ed752940b6d18291e8e1faf
Brehyma Traore <brehymaraore50@gmail.com>
2023-11-28 19:12:04 -0800

    typo fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,8 @@ def uppercase(str):
     new_str = ""
 
     for s in str:
-        if (s.isalpha):
-            new_str += chr(ord(s) - 32) if ('a' <= s <= 'z') elses
+        if (s.isalpha()):
+            new_str += chr(ord(s) - 32) if ('a' <= s <= 'z') else
         else:
             new_str += s
         print("{}".format(new_str))
