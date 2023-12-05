7e34a3f6dcd92e23e05fc3a667ec1472ce8e2f7a
Brehyma Traore <brehymaraore50@gmail.com>
2023-12-05 03:31:53 -0800

    A bit fix

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if 0 > idx > len(my_list):
-        return my_list
-    else:
-        for i in my_list:
-            if i == my_list[idx]:
-                del my_list[idx]
-        return my_list
+    if 0 <= idx < len(my_list):
+        del my_list[idx]
+    return my_list
