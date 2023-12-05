5362884a7e89a4c084163c1c0d13c1e281cf4d71
Brehyma Traore <brehymaraore50@gmail.com>
2023-12-05 01:36:12 -0800

    Some edge case hanled

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    reversed_list = reversed(my_list)
-    for i in reversed_list:
-        print("{:d}".format(i))
+    if not my_list:
+        return None
+    else:
+        reversed_list = reversed(my_list)
+        for i in reversed_list:
+            print("{:d}".format(i))
