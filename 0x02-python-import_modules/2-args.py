1cdf361301854c3380d1e9c22019e05c545dbde2
Brehyma Traore <brehymaraore50@gmail.com>
2023-12-06 14:32:30 -0800

    a program that prints the result of the addition of all arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
         print("{:d} arguments.".format(nb_args))
     elif nb_args == 1:
         print("{:d} argument.".format(nb_args))
+        print("{:d}: {:s}".format(nb_args, argv[nb_args]))
     else:
         print("{:d} arguments:".format(nb_args))
         for i in range(1, nb_args + 1):
