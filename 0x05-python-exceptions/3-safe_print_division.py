b1d8c5078fb12464da60b48dc07fa42e3055bcbc
Brehyma Traore <brehymaraore50@gmail.com>
2023-12-23 22:53:58 -0800

    Division by zero handle

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = a / b
-    except(ZeroDivisionError, ValueError, TypeError):
+        result = int(a) / int(b)
+    except (ZeroDivisionError, TypeError, ValueError):
         result = None
     finally:
         print("Inside result: {}".format(result))
+        return result
