37d526ac4c48772b4b2f220143d44b0ad874e494
Brehyma Traore <brehymatraore50@gmail.com>
2024-03-13 03:00:12 -0700

    Pycodestyle warning fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        print("{:s}".format(chr(ord(c) - 32)) if 'a' <= c <= 'z'
-            else "{:s}".format(c), end='')
+        if 'a' <= c <= 'z':
+            print("{:s}".format(chr(ord(c) - 32)), end='')
+        else:
+            print("{:s}".format(c), end='')
     print()
