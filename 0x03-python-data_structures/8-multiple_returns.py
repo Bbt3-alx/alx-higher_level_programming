467fec41942eef0948385181727df360b4cd7fb8
Brehyma Traore <brehymatraore50@gmail.com>
2024-03-18 15:42:08 -0700

    Hot fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,4 +2,8 @@
 def multiple_returns(sentence):
     if sentence is None:
         first_char = None
-    return len(sentence), sentence[0]
+        s_len = 0
+    else:
+        first_char = sentence[0]
+        s_len = len(sentence)
+    return s_len, first_char
