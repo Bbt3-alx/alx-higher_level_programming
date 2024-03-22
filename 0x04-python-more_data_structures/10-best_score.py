66e212672801f3a19828c9c3d5067a8a857e8eca
Brehyma Traore <brehymatraore50@gmail.com>
2024-03-22 16:02:53 -0700

    Hot fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
-    best_score = 0
+
+    best_score = float('-inf')
+    best_key = None
     for k, v in a_dictionary.items():
         if best_score < v:
             best_score = v
