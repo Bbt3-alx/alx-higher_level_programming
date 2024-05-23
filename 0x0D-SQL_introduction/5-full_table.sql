a9cd0ce949d2dc1eabc1ac4a6e25a740d21d42de
Brehyma Traore <brehymatraore50@gmail.com>
2024-05-23 15:53:49 -0700

    Updated

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,16 @@
 -- a script that prints the full description of the table first_table from the database hbtn_0c_0 in mysql server
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = (SELECT DATABASE()) AND TABLE_NAME = 'first_table'
+USE hbtn_0c_0; -- Use the desired database
+
+
+SELECT 
+	COLUMN_NAME AS 'Field',
+	COLUMN_TYPE AS 'Type',
+	IS_NULLABLE AS 'Null',
+	COLUMN_KEY AS 'Key',
+	COLUMN_DEFAULT AS 'Default',
+	EXTRA AS 'Extra'
+FROM
+	information_schema.COLUMNS
+WHERE
+	TABLE_SCHEMA = DATABASE()
+	AND TABLE_NAME = 'first_table';
