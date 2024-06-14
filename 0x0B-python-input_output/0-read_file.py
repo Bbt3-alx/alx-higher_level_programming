3b50924e666c6cc2ab4f48bf734b7f7290d989e4
Brehyma Traore <brehymatraore50@gmail.com>
2024-06-14 05:00:03 -0700

    A function that reads atext file and prints it to stadout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Module that read a text file """
+
+
+def read_file(filename=""):
+    """
+    A function that reads a text file (UTF-8) and prints it to stdout
+    Args: 
+        filename - The text file to read
+    Return: Nothin
+    """
+    with open(filename, 'r') as f:
+        text = f.read()
+        print(text, end='')
