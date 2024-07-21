cd449fb57cc9098631c0db7bd80cd7428165e2d8
Brehyma Traore <brehymatraore50@gmail.com>
2024-07-21 17:53:53 +0000

    Pycodestyle warning fixed

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,7 +6,7 @@ import json
 
 
 def load_from_json_file(filename):
-    """A function that creates an Object from a 'JSON file'""" 
+    """A function that creates an Object from a 'JSON file'"""
 
     with open(filename, 'r') as f:
         my_data = json.load(f)
