6f016a2ea9ded51bd46d149029081c5ae98c5c16
Brehyma Traore <brehymatraore50@gmail.com>
2024-07-21 17:02:20 +0000

    A function that returns an object represented by a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A module that deserialize"""
+
+
+import json
+
+def from_json_string(my_str):
+    """
+    A function that returns an object (Python data structure)
+    represented by a JSON string"
+    args:
+        my_str - json string to convert
+    """
+
+    my_obj = json.loads(my_str)
+    return my_obj
