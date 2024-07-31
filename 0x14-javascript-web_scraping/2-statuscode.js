41534933005898aa9b60fbc085a8063a1a6d08cd
Brehyma Traore <brehymatraore@gmail.com>
2024-07-31 22:53:03 +0000

    Trailing sapace removed

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,5 +6,5 @@ const url = args[2];
 
 request(url, (error, response, body) => {
   if (error) throw error;
-  console.log('code: ', response.statusCode);
+  console.log('code:', response.statusCode);
 });
