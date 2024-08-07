566798f0719b777cafe558beb0e9af55c5aaf763
Brehyma Traore <brehymatraore50@gmail.com>
2024-08-07 22:43:22 +0000

    A JavaScript script that adds a <li> element to a list when the user clicks on the tag DIV#add_item

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('#red_header').click(function() {
-		$('header').toggleClass('red')
+	$('DIV#red_header').click(function() {
+		$('header').toggleClass('red');
 	});
 });
